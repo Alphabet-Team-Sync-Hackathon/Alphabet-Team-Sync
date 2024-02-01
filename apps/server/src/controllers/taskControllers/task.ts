3c546b90b2f73d3a28478648f00cd18a2bb65e57
ndu ulenu <nduulenu@gmail.com>
2024-02-01 19:37:56 +0100

    admin create task endpoint added

--- apps/server/src/controllers/taskControllers/task.ts
+++ apps/server/src/controllers/taskControllers/task.ts
@@ -1,51 +1,44 @@
-import {Request,Response} from "express";
+import { Request, Response } from "express";
 import { RequestExt } from "../../middlewares/authorization/authentication";
 import Task from "../../models/taskModel/task";
-import { v4 } from 'uuid'
+import { v4 } from "uuid";
 import { HTTP_STATUS_CODE } from "../../constants";
 import Admin from "../../models/adminModel/adminUsers";
 
+export const adminCreateTask = async (req: RequestExt, res: Response) => {
+  try {
+    const { _user: user, _userId: userId, ...rest } = req.body;
+    // const adminId = v4();
+    const confirmId = await Admin.findOne({ where: { id: userId } });
 
-
-export const adminCreateTask=async(req:RequestExt,res:Response)=>{
-    try{
-        const { _user: user, _userId: userId, ...rest } = req.body
-        const adminId = v4()
-const confirmId=await Admin.findOne({where:{id:adminId}})
-
-   if(confirmId){
-const task=await Task.create({
-   id:confirmId.id,
-   title:"",
-   description:"",
-   startTime: new Date(),
-   endTime:new Date(),
-   status: "not_started",
-   employeeId:"",
-   priority: 'low',
-   comments:'',
-   attachments: [],
-   tags: [], 
-   reviewComments: "", 
-   reviewRating:0
-
-    })
-    return res.status(HTTP_STATUS_CODE.SUCCESS).send({
-        message:`Task created successfully`,
-        task
-    })
-
-   }
-   return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).send({
-    message:`User not found`
-   })
-
-    }catch(error){
-        console.log(error);
-        res.status(HTTP_STATUS_CODE.INTERNAL_SERVER).send({
-            message:`There is a problem`
-        })
-        
+    if (confirmId) {
+      const task = await Task.create({
+        id: confirmId.id,
+        title: "",
+        description: "",
+        startTime: new Date(),
+        endTime: new Date(),
+        status: "not_started",
+        employeeId: "",
+        priority: "low",
+        comments: "",
+        attachments: [],
+        tags: [],
+        reviewComments: "",
+        reviewRating: 0,
+      });
+      return res.status(HTTP_STATUS_CODE.SUCCESS).send({
+        message: `Task created successfully`,
+        task,
+      });
     }
-    
-}
+    return res.status(HTTP_STATUS_CODE.UNAUTHORIZED).send({
+      message: `User not found`,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(HTTP_STATUS_CODE.INTERNAL_SERVER).send({
+      message: `There is a problem`,
+    });
+  }
+};
