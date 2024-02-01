2477474fe1519f4eaeeff9ced6a2801b09b241a0
ndu ulenu <nduulenu@gmail.com>
2024-02-01 10:19:11 +0100

    added employee login endpoint

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,47 +1,37 @@
 import express, { Request, Response, NextFunction } from "express";
-import sequelize from './config/dbconfig';
-import employeeRouter from "./routes/employeeRoutes"
+import sequelize from "./config/dbconfig";
+import employeeRouter from "./routes/employeeRoutes";
 
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import logger from "morgan";
 
-
 dotenv.config();
 const app = express();
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
-app.use(logger('dev'))
-app.use(express.json())
+app.use(logger("dev"));
+app.use(express.json());
 
-app.use(cookieParser())
+app.use(cookieParser());
 const port = process.env.PORT || 3000;
-app.use("/employee",employeeRouter);
+app.use("/employee", employeeRouter);
 
-
-
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, Express with TypeScript!");
 });
 
 sequelize
   .sync({
     //force:true
-  }) 
+  })
   .then(() => {
     console.log("Database connected Successfully");
   })
   .catch((error) => {
-    console.error('Database sync error:', error);
+    console.error("Database sync error:", error);
   });
 
-app.listen(port ,() => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-
-
-
--- apps/server/src/config/env.ts
+++ apps/server/src/config/env.ts
@@ -1,19 +1,4 @@
-import dotenv from 'dotenv'
 
-// Check condition to determine which environment to use
-const isProduction = process.env.NODE_ENV === 'production'
-
-// Specify the path to the .env file
-const envFilePath = isProduction ? '.env.production' : '.env.development'
-console.log(`running in ${process.env.NODE_ENV} mode`)
-
-// Load the environment variables from the specified file
-const result = dotenv.config({ path: envFilePath })
-
-if (result.error) {
-  console.error(`Failed to load environment variables from ${envFilePath}`)
-  process.exit(1)
-}
 
 const {
   PORT,
@@ -51,7 +36,7 @@ const ENV = {
   OAUTH_CLIENT_ID,
   OAUTH_CLIENT_SECRET,
   REDIS,
-  IS_PROD: isProduction,
+  // IS_PROD: isProduction,
   FE_BASE_URL,
   CLOUDINARY_NAME,
   API_KEY_4_CLOUDINARY,
