5cc65102229db0469f7e96e3526235368474f041
balijawa hussein <balijawahussein@gmail.com>
2024-02-02 07:08:14 +0300

    l

--- apps/client/src/components/section-3.jsx
+++ apps/client/src/components/section-3.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Sidenav from "./sidenav";
 import Header from "./headeing";
-
+import Card from "./card";
 const Section3 =()=>
 {
     return(<section className="sect-3">
@@ -11,6 +11,9 @@ const Section3 =()=>
         <div className="s2">
             <div className="s2-top">
             <Header title="Activity Log" fontSize="25px" textColor="#f7f7f7" />
+            <div className="s2-top-infill">
+            <Card number ='20' />
+            </div>
             </div>
             <div className="s2-bottop"></div>
         </div>
